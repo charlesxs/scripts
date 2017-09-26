d62eae55120b8aacac2793daa0253b07cdf80619
xiaoshuang <huiran_shi@126.com>
2017-09-26 11:50:16 +0800

    insert sort

---
+++ go/insert-sort.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func InsertSort(datalist []int) []int {
+	var j int
+
+	for i, d := range datalist {
+		if i == 0 {
+			continue
+		}
+
+		j = i
+		for j > 0 && datalist[j-1] > d {
+			datalist[j] = datalist[j-1]
+			j--
+		}
+		datalist[j] = d
+	}
+
+	return datalist
+}
+
+
+func main()  {
+	list := []int{2, 5, 3, 7, 10, 8, 4, 1, 100, 5, 100, 200, -1, -3}
+
+	fmt.Println(InsertSort(list))
+}
+
