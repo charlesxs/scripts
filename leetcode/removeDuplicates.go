fd10766ac22f62f719bb3f1df30e673fc52b96df
xiaoshuang <huiran_shi@126.com>
2017-12-27 18:08:04 +0800

    add remove dumplicates

---
+++ leetcode/removeDuplicates.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+func RemoveDumplicates(list []int ) int {
+	length := len(list)
+	if length == 0 {
+		return length
+	}
+
+	var i int
+	for j := 1; j < length; j++ {
+		if list[i] != list[j] {
+			i++
+			list[i] = list[j]
+		}
+	}
+
+	return  i + 1
+}
+
+
+func main() {
+	list := []int{1, 3, 3, 5, 6, 9, 9, 9, 9, 10, 10, 11}
+
+	r := RemoveDumplicates(list)
+	fmt.Println(list[:r], r)
+}
+
+
+// 问题描述: https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+//
+
